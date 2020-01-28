fa5ae566f090da97ad21c8b41fc689935ec84d1d
Josh Rosenbaum <34691747+joshrosenbaum-dev@users.noreply.github.com>
2020-01-27 22:36:57 -0500

    Update pandas_csv.py

--- pandas_csv.py
+++ pandas_csv.py
@@ -1,16 +1,15 @@
-import pandas as pd     # install: python pip pandas
+import pandas as pd
+    # install: python pip pandas
 
 x_file = "csv/income.csv"
 y_file = "csv/life_expectancy.csv"
     # We would use the <self.file> attribute of the marker to get 
     # this data from the X bucket's marker and Y bucket's marker, respectively.
 year = "2018"
-country = "Afghanistan"
-
-x = pd.read_csv(x_file)
-y = pd.read_csv(y_file)
-x_val = x[x.country == country][year][0]    # Sourced from: https://data36.com/pandas-tutorial-1-basics-reading-data-files-dataframes-data-selection/
-y_val = y[y.country == country][year][0]
-
-data_pair = [x_val, y_val]
-print(data_pair)
+country = "United States"
+x = pd.read_csv(x_file, index_col = "country")[year].loc[country]
+y = pd.read_csv(y_file, index_col = "country")[year].loc[country]
+    # https://stackoverflow.com/questions/20107570/removing-index-column-in-pandas-when-reading-a-csv
+    # https://brohrer.github.io/dataframe_indexing.html
+pair = [x, y]
+print(pair)
