6b237806002a9e580a5306b0b1b4853ef42e2cd0
Josh Rosenbaum <34691747+joshrosenbaum-dev@users.noreply.github.com>
2020-02-05 14:48:29 -0500

    Update pandas_csv.py

--- pandas_csv.py
+++ pandas_csv.py
@@ -6,5 +6,5 @@ def get_plot(mark_x, mark_y, mark_c):
     y = pd.read_csv(mark_y.file, index_col = "country")[year].loc[mark_c.country]
         # https://stackoverflow.com/questions/20107570/removing-index-column-in-pandas-when-reading-a-csv
         # https://brohrer.github.io/dataframe_indexing.html
-    pair = [x, y]
+    pair = [x, y, mark_c.country]
     return pair
