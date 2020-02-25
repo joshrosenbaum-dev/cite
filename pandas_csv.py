f154cc92b5d43768102971810b05e7cba493fa7b
joshrosenbaum-dev <34691747+joshrosenbaum-dev@users.noreply.github.com>
2020-02-25 11:18:04 -0500

    Refactor react.py

--- pandas_csv.py
+++ pandas_csv.py
@@ -2,9 +2,9 @@ import pandas as pd
 def get_plot(mark_x, mark_y, mark_c):
     #TODO: Make not constant
     year = "2018"
-    x = pd.read_csv(mark_x.file, index_col = "country")[year].loc[mark_c.country]
-    y = pd.read_csv(mark_y.file, index_col = "country")[year].loc[mark_c.country]
+    x = pd.read_csv(mark_x.file, index_col = "country")[year].loc[mark_c.label]
+    y = pd.read_csv(mark_y.file, index_col = "country")[year].loc[mark_c.label]
         # https://stackoverflow.com/questions/20107570/removing-index-column-in-pandas-when-reading-a-csv
         # https://brohrer.github.io/dataframe_indexing.html
-    pair = [x, y, mark_c.country]
+    pair = [x, y, mark_c.label]
     return pair
--- react.py
+++ react.py
@@ -2,64 +2,85 @@ from kivy.app import App
 from kivy.config import Config
 from kivy.uix.widget import Widget
 from kivy.storage.jsonstore import JsonStore
-from random import random
 from pandas_csv import get_plot
 
 class Marker:
-    def __init__(self, fid): # , touch):
+    def __init__(self, fid):
+        # Retrieve ID based on touch.fid, position based on touch.pos
         self.fid = fid
-        self.x = 100. # touch.pos[0]
-        self.y = 100. # touch.pos[1]
-        self.color = (random(), random(), random())
-        
-        self.is_x = self.is_y = self.is_time = False
-        self.icon = self.axis_label = self.file = self.country = None
+        self.pos = [100.0, 100.0]
+        self.type = self.indicator_id = self.artifact_id = self.label = self.file = self.icon = None
+        self.is_x = self.is_y = self.is_time = False       
 
-        attributes = JsonStore("attributes.json")   # Sourced from: https://kivy.org/doc/stable/api-kivy.storage.jsonstore.html 
-        if attributes.exists(str(self.fid)):
-            if attributes.get(str(self.fid)).get("is_x"):
+        # TODO: Move to loadData in MarkerHandler
+        attributes = JsonStore("attributes.json")
+        indicators = JsonStore("indicators.json")
+        artifacts = JsonStore("artifacts.json")
+
+        fid_str = str(self.fid)
+
+        # Check if ID is in attributes table
+        if attributes.exists(fid_str):
+            attribute = attributes.get(fid_str)
+            if attribute.get("is_x"):
+                self.type = "X"
                 self.is_x = True
-            elif attributes.get(str(self.fid)).get("is_y"):
+            elif attribute.get("is_y"):
+                self.type = "Y"
                 self.is_y = True
-            elif attributes.get(str(self.fid)).get("is_time"):
+            elif attribute.get("is_time"):
+                self.type = "Time"
                 self.is_time = True
-            elif attributes.get(str(self.fid)).get("indicator"):
-                indicators = JsonStore("indicators.json")
-                indicator = indicators.get(attributes.get(str(self.fid)).get("indicator"))
-                # TODO: Save indicator ID
-                # TODO: Make a markerType field
-                self.axis_label = indicator.get("label")
-                self.file = "csv/" + indicator.get("file") # change for dataframe
-            elif attributes.get(str(self.fid)).get("country"):
-                countries = JsonStore("countries.json")
-                country = countries.get(attributes.get(str(self.fid)).get("country"))
-                self.country = country.get("label")
-                self.icon = "icons/" + country.get("abbr") + ".png"
+            elif attribute.get("indicator"):
+                indicator = indicators.get(attribute.get("indicator"))
+                self.indicator_id = attribute.get("indicator")
+                self.type = "Indicator"
+                self.label = indicator.get("label")
+                # TODO: Match indicator to preloaded dataframe vs. file
+                self.file = "csv/" + indicator.get("file")
+            elif attribute.get("artifact"):
+                artifact = artifacts.get(attribute.get("artifact"))
+                self.artifact_id = attribute.get("artifact")
+                self.type = "Artifact"
+                self.label = artifact.get("label")
+                self.icon = "icons/" + artifact.get("abbr") + ".png"
+   
+    def to_string(self):
+        print(self.fid,"\t", end = "")
+        print(self.is_x,"\t", end = "")
+        print(self.is_y,"\t", end = "")
+        print(self.is_time,"\t", end = "")
+        print("(" + format(self.pos[0], '.2f') + ", " + format(self.pos[1], '.2f') + ")\t", end = "")
+        print(self.indicator_id,"\t\t", end = "")
+        print(self.artifact_id,"\t\t", end = "")
+        print(self.type, end = " ")
+        print("(" + format(self.label) + ")","\t")
 
-class RVHandler(Widget):
+class MarkerHandler(Widget):
     markers_ontable = [Marker(0), Marker(1), Marker(2), Marker(11), Marker(12), Marker(21), Marker(22), Marker(24)]
-    #markers_ontable = []
-
+    # markers_ontable = []
+    
     def loadData(self):
         print("Loading")
         self.indicatorData = {}
         # Key --> indicator "0", "1"
-        # Data --> Pandas dataframe, based on filename generated from json
+        # Data --> Pandas dataframe, based on filename generated from JSON
 
     def on_touch_down(self, touch):
-        print("On touch down") # put down all markers, see if on touch down prints before loading
-        self.table_status()
+        # Put down all markers, see if on touch down prints before loading
+        print("On touch down")
+        self.table_init()
         if "markerid" in touch.profile:
             marker = Marker(touch)
             self.markers_ontable.append(marker)
-            # self.table_status() # TODO: Rename this function as the table initializer.
+            # self.table_init()
 
     def on_touch_up(self, touch):
         if "markerid" in touch.profile:
             for marker in self.markers_ontable:
                 if marker.fid == touch.fid:
                     self.markers_ontable.remove(marker)
-            self.table_status()
+            self.table_init()
 
     def on_touch_move(self, touch):
         if "markerid" in touch.profile:
@@ -67,70 +88,50 @@ class RVHandler(Widget):
                 if marker.fid == touch.fid:   
                     marker.x = touch.x
                     marker.y = touch.y
-    
-    def table_status(self):
-        attributes = JsonStore("attributes.json")
-        indicators_mot, countries_mot, end_plots = [], [], []
 
-        print("\nID\t IS_X\t IS_Y\t TIME\t POSITION\t\t INDICATOR_ID\t COUNTRY_ID\t NOTES")
+    def table_init(self):
+        attributes = JsonStore("attributes.json")
+        indicators_mot, artifacts_mot, end_plots = [], [], []
+
+        print("\nID\tIS_X\tIS_Y\tTIME\tPOSITION\t\tINDICATOR_ID\tCOUNTRY_ID\tTYPE (LABEL)")
         print("===================================================================================================================")
         if len(self.markers_ontable) == 0:
             print("N/A\t No markers are on the table ------------------------------------------------------------------------------")
-        for index, marker in enumerate(self.markers_ontable): 
+        for index, marker in enumerate(self.markers_ontable):
             if attributes.exists(str(marker.fid)):
-                indicator = attributes.get(str(marker.fid)).get("indicator")
-                country = attributes.get(str(marker.fid)).get("country")
-                if indicator:
+                if marker.type == "Indicator":
                     indicators_mot.append(index)
-                if country:
-                    countries_mot.append(index)
-
-        ##########################################################################################
-                print(marker.fid,"\t",attributes.get(str(marker.fid)).get("is_x"), end = "")
-                print("\t",attributes.get(str(marker.fid)).get("is_y"), end = "")
-                print("\t",attributes.get(str(marker.fid)).get("is_time"), end = "")
-                print("\t (" + format(marker.x, '.2f') + ", " + format(marker.y, '.2f') + ")", end ="")
-                print("\t",indicator, end = "")
-                print("\t\t",country, end = "")
-                if marker.is_x:
-                    print("\t\t","X Bucket")
-                elif marker.is_y:
-                    print("\t\t","Y Bucket")
-                elif marker.is_time:
-                    print("\t\t","Time Dial")
-                elif marker.axis_label is not None:
-                    print("\t\t",marker.axis_label)
-                elif marker.country is not None:
-                    print("\t\t",marker.country)
+                if marker.type == "Artifact":
+                    artifacts_mot.append(index)
+                marker.to_string()
             else:
                 print(marker.fid,"\t ----------------------------------------------------------------------------------------------------------")
-        ##########################################################################################
 
         print("\nINDICATORS_MOT:",indicators_mot,len(indicators_mot))
-        print("COUNTRIES_MOT:",countries_mot,len(countries_mot))
+        print("COUNTRIES_MOT:",artifacts_mot,len(artifacts_mot))
 
-        if len(indicators_mot) >= 2 and len(countries_mot) >= 1:
+        if len(indicators_mot) >= 2 and len(artifacts_mot) >= 1:
             x = indicators_mot[0]
             y = indicators_mot[1]
-            for mot_entry in countries_mot:
+            for mot_entry in artifacts_mot:
                 # firstData = indicatorData[self.markers_ontable[x].indID]
                 end_plots.append(get_plot(self.markers_ontable[x], self.markers_ontable[y], self.markers_ontable[mot_entry]))
 
         print(end_plots)
 
-class RVApp(App):
+class ReactivisionApp(App):
     def build(self):
         Config.set("input", "reactivision", "tuio,0.0.0.0:3333")
-        # start deletable configuration
+        # Deletable configuration for window sizing
         Config.set("graphics", "width", "640")
         Config.set("graphics", "height", "480")
         Config.set("graphics", "position", "custom")
         Config.set("graphics", "left", 850)
         Config.set("graphics", "top",  100)
-        # end deletable configuration
-        handler = RVHandler()
-        handler.loadData() # maybe put a sleep function after this to avoid overlap with touch -- load all CSV files
-        return handler
+        Handler = MarkerHandler()
+        Handler.loadData() 
+        # TODO: Sleep function to avoid overlap w/ touch
+        return Handler
 
 if __name__ == "__main__":
-    RVApp().run()
+    ReactivisionApp().run()
