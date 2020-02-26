2f1c1b84ed87619943160b368723a5c1be11c3f7
Josh Rosenbaum <34691747+joshrosenbaum-dev@users.noreply.github.com>
2020-02-25 19:01:51 -0500

    Remove unnecessary comments

--- mpl.py
+++ mpl.py
@@ -1,28 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-
 DEBUG_Y = 0
-
 def plotPoints(points, mark_x, mark_y):
     global DEBUG_Y
     plt.clf()
+    plt.suptitle(mark_x.label + " vs. " + mark_y.label)
     plt.xlabel(mark_x.label)
     plt.ylabel(mark_y.label)
-    # TODO: Why don't my points show up???
     xc = []
     yc = []
     for p in points:
         xc.append(p[0])    
         yc.append(p[1])
-        #plt.plot(p[0], p[1])        
         print(p)
-
     yc[-1] += DEBUG_Y
-
     plt.plot(xc, yc, 'bo')
-
     DEBUG_Y += 1.0
-    #plt.suptitle("CITE Matplotlib Test")
-    
-    #plt.show()
