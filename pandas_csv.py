36b1e0c1ef50e55a21b64cd91c1989c9dab803dd
Josh Rosenbaum <34691747+joshrosenbaum-dev@users.noreply.github.com>
2020-02-25 19:36:52 -0500

    Remove old function

--- pandas_csv.py
+++ pandas_csv.py
@@ -1,15 +1,5 @@
 import pandas as pd
 
-def getPoint2(mark_x, mark_y, mark_c):
-    #TODO: Make not constant
-    year = "2018"
-    x = pd.read_csv(mark_x.file, index_col = "country")[year].loc[mark_c.label]
-    y = pd.read_csv(mark_y.file, index_col = "country")[year].loc[mark_c.label]
-        # https://stackoverflow.com/questions/20107570/removing-index-column-in-pandas-when-reading-a-csv
-        # https://brohrer.github.io/dataframe_indexing.html
-    point = [x, y, mark_c.label]
-    return point
-
 def getPoint(xFrame, yFrame, artifactLabel):
     # TODO: Make year not constant.
     year = "2018"
