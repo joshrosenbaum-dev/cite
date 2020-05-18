ee220f0942656b5e8dc5b0e8359fb33c10d1fbb1
Josh Rosenbaum <34691747+joshrosenbaum-dev@users.noreply.github.com>
2020-05-18 17:59:58 -0400

    Cleanup data collection for markers

--- marker.py
+++ marker.py
@@ -13,15 +13,41 @@
 #   https://kivy.org/doc/stable/guide/inputs.html
 #   https://kivy.org/doc/stable/api-kivy.input.motionevent.html
 
+from enum import Enum
+
+class MarkerType(Enum):
+    X = "X Bucket"
+    Y = "Y Bucket"
+    TIME = "Time Dial"
+    ARTIFACT = "Artifact"
+    INDICATOR = "Indicator"
+    
 class Marker:
-    def __init__(self, touch, jsonData):
+    def __init__(self, id, markerData):
         #   Class variables based on touch profile:
-        self.currentPos = touch.pos
-        self.fidicualID = touch.fid
+        self.currentPos = (0.0, 0.0)
+        self.fidicualID = id
 
         #   Custom class variables:
         self.artifactID = None
         self.artifactAbbr = None
+        self.audio = None
         self.indicatorID = None
         self.markerLabel = None
         self.markerType = None
+
+        if format(id) in markerData:
+            mD = markerData[format(id)]
+            self.audio = mD["audio"]
+            self.markerLabel = mD["label"]
+            self.markerType = mD["type"]
+            if self.markerType == MarkerType.ARTIFACT:
+                self.artifactID = mD["artifact_id"]
+                self.artifactAbbr = mD["artifact_abbr"]
+            if self.markerType == MarkerType.INDICATOR:
+                self.indicatorID = mD["indicator_id"]
+
+    def __str__(self):
+        return "[{}, {}, {}]\t\t{}\t{}\t{}\t\t{}".format(
+            self.fidicualID, self.indicatorID, self.artifactID, self.currentPos, self.markerType.value, self.audio, self.markerLabel
+        )
